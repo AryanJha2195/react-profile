6fc761d5c1454e3d0d8c393e48dc98f68c42a41a
Aryan <aryan2195jha@gmail.com>
2023-01-21 16:38:26 +0530

    Initial Commit: Basic App

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import Nav from './component/nav';
+import About from './component/about';
+import Header from './component/header';
+import Experience from './component/experience';
+import Services from './component/services';
+import Portfoilo from './component/portfoilo';
+import Contact from './component/contact';
+import Footer from './component/footer';
+
+function App() {
+  return (
+    <>
+    <Nav/>
+    <Header/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfoilo/>
+    <Contact/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;

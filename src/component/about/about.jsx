9e0ac486ab9e67893ea57dbbac72d6c8f9a37baf
Aryan <aryan2195jha@gmail.com>
2023-01-25 01:09:17 +0530

    card design

--- src/component/about/about.jsx
+++ src/component/about/about.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { useState } from 'react'
 import './about.css'
-import 'react-icons/fa' 
+import 'react-icons/fa'
 import { FaBrush, FaGit, FaTimes } from 'react-icons/fa'
+import { BsFillBrushFill } from 'react-icons/bs'
+import { BiGitRepoForked } from 'react-icons/bi'
+import { GiSandsOfTime } from 'react-icons/gi'
 const data = {
-  "Experience": "2+ year of working",
+  "Experience": "2 + year of working",
   "Projects": "10+ Projects",
   "Skills": ["React", "Angular", "NodeJS"],
   "Description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, nobis! Nulla, aspernatur iure. Fugit asperiores nobis tempora voluptatum, ex explicabo eos possimus culpa magni, nulla ipsam facilis inventore nihil voluptatem est obcaecati quae in ipsa voluptatibus, autem tempore officiis? Maxime, dolorum! Optio, architecto. Doloremque, illo?"
 }
-
-
-
 
 const About = () => {
 
@@ -29,7 +29,6 @@ const About = () => {
 
       <div className='container1'>
 
-
         <div className='left'>
           <img src={process.env.PUBLIC_URL + '/images/person.webp'} alt="404 NOT FOUND" className="sideimg" />
         </div>
@@ -37,9 +36,27 @@ const About = () => {
 
         <div className='right'>
           <div className='cards'>
-            <div className="card">Experience<FaTimes/></div>
-            <div className="card">Projects<FaGit/></div>
-            <div className="card">Skills<FaBrush/></div>
+            <div className="card">
+              <div className="icons">
+              <div className='icon'><GiSandsOfTime /></div>
+              <div>Experience</div>
+              <div><small>{experience}</small></div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="icons">
+              <div className='icon'><BiGitRepoForked /></div>
+              <div>Projects</div>
+              <div><small>{projects}</small></div>
+              </div>
+            </div>
+            <div className="card">
+              <div className="icons">
+              <div className='icon'><BsFillBrushFill /></div>
+              <div>Skills</div>
+              <div><small>{skills.map((skill)=>  skill + " ")}</small></div>
+              </div>
+            </div>
           </div>
 
           <div className='aboutmecontent'>
